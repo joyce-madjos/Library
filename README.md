eb08074ffde9114630fa5b094a9101b1d3d5fa5c
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-04-24 20:29:34 +0800

    Added modal for asking new book

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Library
 
 Background Photo by Ivo Rainha from Pexels: https://www.pexels.com/photo/assorted-books-on-shelf-1290141/
+
+.modal {
+  /* display: none; */
+  /* max-width: -1px; */
+  /* height: 100%; */
+  /* margin: 0 auto; */
+  /* left: 500px; */
+  /* top: 0; */
+  position: absolute;
+  background-color: var(--licorice);
+  color: black;
+  overflow: auto;
+  z-index: 1;
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: 2fr 5fr;
+  /* min-height: 100svh; */
+  max-width:max(100%, 50vw) ; 
+}
+
+.modal-content {
+  position: relative;
+  background-color: #fefefe;
+  padding:2rem ;
+  border-radius: 1rem;
+  border: 1px solid #888;
+  cursor: context-menu;
+  margin-top: 40%;
+  /* margin: 10rem auto 5rem; */
+  /* max-width: 100%; */
+}
--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-// Create an object named Book with author, title, page and status properties and methods to read and not read
-// Create a function that will return a string depending on the status of the book
-// Create an array of several books
-// Loop through the array and display the books on the page
+let bookTitle = document.getElementById("book-title");
 
-// Create an object named Book with author, title, page and status properties and methods to read and not read
+
+
+
+// Get the modal
+let modal = document.getElementById("the-modal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("new-book");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+
+// An object named Book with author, title, page and status properties and methods to read and not read
 function Book(author, title, pages, status) {
   this.author = author;
   this.title = title;
@@ -14,7 +47,7 @@ function Book(author, title, pages, status) {
   };
 }
 
-// Create an array of several books
+// An array of several books
 let books = [
     new Book("J.K. Rowling", "Harry Potter", 400, "not read"),
     new Book("J.R.R. Tolkien", "Lord of the Rings", 700, "read"),
@@ -25,5 +58,5 @@ let books = [
     new Book("J.K. Rowling", "Harry Potter and the Prisoner of Azkaban", 600, "read"),
 ];
 
-// display the books on the page using console.log
+// Display the books on the page using console.log
 books.forEach(book => console.log(book.info()));
