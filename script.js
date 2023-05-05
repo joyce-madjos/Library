b8f5d1bd4c1dc964c7b0357e0f8f0ca39809cb87
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-05-05 22:33:39 +0800

    Systematized the process

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let bookAuthor = document.getElementById("book-author");
 let bookPages = document.getElementById("book-page");
 let bookStatus = document.getElementById("book-status");
 let switchBtn = document.getElementById("switch");
-let card = document.getElementById("card");
+let emptyCard = document.getElementById("card");
+let cardHolder = document.getElementById("card-holder");
+let rightSide = document.getElementById("right-side");
+let leftSide = document.getElementById("left-side");
+let container = document.getElementById("container");
 let circle = document.getElementById("circle");
 let newTitle = document.getElementById("new-title");
 let newAuthor = document.getElementById("new-author");
@@ -11,6 +15,7 @@ let newPages = document.getElementById("new-pages");
 let newStatus = document.getElementById("new-status");
 let modal = document.querySelector(".modal");
 let addBookButton = document.getElementById("addBook");
+var myLibrary = [];
 
 // function constructor for the Book object
 function Book(author, title, pages, status) {
@@ -24,14 +29,14 @@ function Book(author, title, pages, status) {
 }
 
 // function to change the status of the book when read
-function read() {
+function read(card, circle, bookStatus) {
   bookStatus.innerText = "Read";
   card.style.backgroundColor = "rgba(0, 128, 0, 0.5)";
   circle.style.backgroundColor = "rgba(17, 234, 46, 0.5)";
 }
 
 // function to change the status of the book when unread
-function unread() {
+function unread(card, circle, bookStatus) {
   bookStatus.innerText = "Unread";
   card.style.backgroundColor = "rgba(222, 27, 27, 0.5)";
   circle.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
@@ -40,84 +45,105 @@ function unread() {
 // add event listener to the add book button
 addBookButton.addEventListener("click", function () {
 
-  // create a new element for the new book
-  let newBookCard = document.createElement("div");
-  newBookCard.classList.add("card");
-  let newBookTitle = document.createElement("h2");
-  newBookTitle.classList.add("book-name");
-  let newBookAuthor = document.createElement("h3");
-  newBookAuthor.classList.add("book-author");
-  let newBookPages = document.createElement("h3");
-  newBookPages.classList.add("book-page");
-  let newBookStatus = document.createElement("h3");
-  newBookStatus.classList.add("book-status");
-  let newBookSwitch = document.createElement("label");
-  newBookSwitch.classList.add("switch");
-  let newBookInput = document.createElement("input");
-  newBookInput.setAttribute("type", "checkbox");
-  newBookInput.setAttribute("id", "switch");
-  let newBookSpan = document.createElement("span");
-  newBookSpan.classList.add("slider");
-  newBookSpan.classList.add("round");
-  let newBookDelete = document.createElement("button");
-  newBookDelete.classList.add("delete");
-  newBookDelete.innerText = "Delete";
+  emptyCard.style.display = "none";
 
-  // append the new book to the page
-  newBookSwitch.appendChild(newBookInput);
-  newBookSwitch.appendChild(newBookSpan);
-  newBookCard.appendChild(newBookTitle);
-  newBookCard.appendChild(newBookAuthor);
-  newBookCard.appendChild(newBookPages);
-  newBookCard.appendChild(newBookStatus);
-  newBookCard.appendChild(newBookSwitch);
-  newBookCard.appendChild(newBookDelete);
-  document.body.appendChild(newBookCard);
-
-  // get the values from the input fields
-  let newAuthor = document.getElementById("new-author");  
-  let newTitle = document.getElementById("new-title");
-  let newPages = document.getElementById("new-pages");
-  let newStatus = document.getElementById("new-status");
-
-  // LEGIT UNG MGA NASA BABA |
-  //                         V
-
-  // create a new book object
-  let newBook = new Book(
-    newAuthor.value,
-    newTitle.value,
-    newPages.value,
-    newStatus.value
+  myLibrary.push(
+    new Book(newAuthor.value, newTitle.value, newPages.value, newStatus.value)
   );
-  // display the new book on the page
-  bookTitle.innerText = newBook.title;
-  bookAuthor.innerText = newBook.author;
-  bookPages.innerText = newBook.pages;
-  // get the status of the new book from dropdown
-  if (newStatus.value == "1") {
-    switchBtn.checked = true;
-    read();
-  } else if (newStatus.value == "2") {
-    switchBtn.checked = false;
-    unread();
-  }
-  // clear the input fields after adding a new book
-  newTitle.value = "";
-  newAuthor.value = "";
-  newPages.value = "";
-  newStatus.value = "";
 
+  let index = myLibrary.length - 1;
+  createCard(
+    myLibrary[index].title,
+    myLibrary[index].author,
+    myLibrary[index].pages,
+    myLibrary[index].status
+  );
+
+  createTitleCard(myLibrary[index].title);
 });
 
-// change the status of the book when the switch is clicked
-switchBtn.addEventListener("click", function () {
-  if (switchBtn.checked == true) {
-    read();
-  } else {
-    unread();
+var titleHolder = document.getElementById("title-holder");
+
+function createTitleCard(title) {
+
+  let titleCard = document.createElement("p");
+  titleCard.classList.add("title");
+  titleCard.textContent = title;
+
+  // container.appendChild(leftSide);
+  // leftSide.appendChild(titleHolder);
+  titleHolder.appendChild(titleCard);
+
+   document.body.appendChild(container);
+  // container.appendChild(titleCard);
+}
+
+// function to create a card for the book
+function createCard(title, author, page, status) {
+  //  create the elements
+  let card = document.createElement("div");
+  card.classList.add("card");
+  let circle = document.createElement("div");
+  circle.classList.add("circle");
+  let bookBody = document.createElement("div");
+  bookBody.classList.add("book-body");
+  let bookName = document.createElement("p");
+  bookName.classList.add("book-name");
+  let bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("author");
+  let bookPages = document.createElement("p");
+  bookPages.classList.add("pages");
+  let bookStatus = document.createElement("p");
+  bookStatus.classList.add("status");
+  let switchLabel = document.createElement("label");
+  switchLabel.classList.add("switch");
+  let switchInput = document.createElement("input");
+  switchInput.setAttribute("type", "checkbox");
+  let switchSpan = document.createElement("span");
+  switchSpan.classList.add("slider");
+
+  // append the elements to the document
+  container.appendChild(rightSide);
+  rightSide.appendChild(cardHolder);
+  cardHolder.appendChild(card);
+  card.appendChild(circle);
+  card.appendChild(bookBody);
+  bookBody.appendChild(bookName);
+  bookBody.appendChild(bookAuthor);
+  bookBody.appendChild(bookPages);
+  bookBody.appendChild(bookStatus);
+  card.appendChild(switchLabel);
+  switchLabel.appendChild(switchInput);
+  switchLabel.appendChild(switchSpan);
+
+  // set the text content of the book name, author, pages, and status elements
+  bookName.textContent = title;
+  bookAuthor.textContent = author;
+  bookPages.textContent = page + " pages";
+
+  // set the status of the book
+  if (status == "1") {
+    switchInput.checked = true;
+    read(card, circle, bookStatus);
+  } else if (status == "2") {
+    switchInput.checked = false;
+    unread(card, circle, bookStatus);
   }
-});
+
+  switchInput.addEventListener("click", function () {
+    if (switchInput.checked == true) {
+      read(card, circle, bookStatus);
+    } else {
+      unread(card, circle, bookStatus);
+    }
+  });
+
+  // append the card to the document body
+  document.body.appendChild(container);
+}
+
+
+
 
 // ------------------------------
 
